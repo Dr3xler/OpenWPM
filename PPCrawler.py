d237adcfa38dd1c2f8cd94f4427fe600fc5d6561
Sebastian Ewald <sebastian.ewald1@gmx.de>
2021-04-30 13:10:56 +0200

    PP commit

--- PPCrawler.py
+++ PPCrawler.py
@@ -1,22 +1,43 @@
 
 from automation import CommandSequence, TaskManager
-
+import csv
 
 # The list of sites that we wish to crawl
-NUM_BROWSERS = 2
-sites = ['https://www.java.com/',
-	'https://www.google.de/']
+NUM_BROWSERS = 5
+
+#loading the .csv file into an array
+sites = []
+
+with open('/home/pp/OpenWPM/csv/TOP100.csv') as csvDataFile:
+	csvReader = csv.reader(csvDataFile)
+	for row in csvReader:
+		sites.append(row[0])
+
+#if array[0] != "http://www.google.de":
+
+sites.pop(0)
+sites.insert(0,'http://www.google.de')
+
+
+
+
+# OLD:
+#sites = ['https://www.java.com/',
+#	'https://www.google.de/']
+
+
 
 # Loads the default manager params
 # and NUM_BROWSERS copies of the default browser params
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 
 # Update browser configuration (use this for per-browser settings)
+# HTTP Requests and Responses are set to False due to cookie only search
 for i in range(NUM_BROWSERS):
     # Record HTTP Requests and Responses
     browser_params[i]['http_instrument'] = True
     # Record cookie changes
-    browser_params[i]['cookie_instrument'] = True
+    browser_params[i]['cookie_instrument'] = False
     # Record Navigations
     browser_params[i]['navigation_instrument'] = True
     # Record JS Web API calls
@@ -39,9 +60,13 @@ for site in sites:
     # Parallelize sites over all number of browsers set above.
     # (To have all browsers go to the same sites, add `index='**'`)
     command_sequence = CommandSequence.CommandSequence(site, reset=True)
-
+    
     # Start by visiting the page
+
     command_sequence.get(sleep=3, timeout=60)
+    command_sequence.recursive_dump_page_source()
+    #command_sequence.recursive_dump_page_source() #for recursive Top Level Frame incl. all nested iframes
+
 
     # Run commands across the three browsers (simple parallelization)
     manager.execute_command_sequence(command_sequence)
